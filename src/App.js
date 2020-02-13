4ad25b022578ada3434837b57b56de358b5ac6fc
Clark Tyra <clark.tyra@gmail.com>
2020-02-13 14:47:44 -0800

    finished a navbar component, a navbar.js file and a navbar.scss file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Navbar from './components/Navbar/Navbar.js'
 
 
 function App() {
   return (
-    <h1>the current app</h1>
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+    
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from '../../logo.svg'
+import './navbar.scss'
+
+export default function Navbar(){
+    return (
+        <nav className="navBar">
+            <img src={logo} alt="city tours company" />
+            <ul className="nav-links">
+                <li>
+                    <a href="/" className="nav-link"> 
+                        home
+                    </a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link"> 
+                        about
+                    </a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link active"> 
+                        tours
+                    </a>
+                </li>
+            
+            </ul>
+        </nav>
+
+    )
+}
+
